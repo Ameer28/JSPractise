947bbaab5827a1160e16cf4ac112c53e177ab1b9
Ameer Pestana <elearningviet88@gmail.com>
2020-08-04 17:20:54 +0800

    Ternary Operator and Switch Statements
    Ternary Operator and Switch Statements

--- script.js
+++ script.js
@@ -69,9 +69,161 @@
 
 /********************Operator Procedence **************************/
 
-var now = 2018;
-var yearJohn = 1989;
-var fullAge = 18;
+// var now = 2018;
+// var yearJohn = 1989;
+// var fullAge = 18;
 
-var isFullAge = now - yearJohn >= fullAge;
-console.log(isFullAge);
+// // Multiple operators
+// var isFullAge = now - yearJohn >= fullAge;
+// console.log(isFullAge);
+
+// // Grouping
+// var ageJohn = now - yearJohn;
+// var ageMark = 35;
+// var average = (ageJohn + ageMark) / 2;
+// console.log(average);
+
+// // Multiple assignments
+// var x, y;
+// x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+// console.log(x, y);
+
+// // More operators
+// x *= 2;
+// console.log(x);
+// x += 10;
+// console.log(x);
+// x--;
+// console.log(x);
+
+/************************Coding Challenge 1 ************************/
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height).(mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs
+3. Create a boolean variable containing information about whether Mark has a highter BMI than John
+4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's> true").
+*/
+
+// Mark's and John's Mass & Height
+// var massMark = 70;
+// var massJohn = 85;
+// var heightMark = 1.7;
+// var heightJohn = 1.8;
+
+// // Both their BMIs
+// var bmiMark = massMark / (heightMark^2);
+// console.log(bmiMark);
+
+// var bmiJohn = massJohn / (heightJohn^2);
+// console.log(bmiJohn);
+
+// // Is Mark's BMI more than John's?
+// moreBmi = bmiMark > bmiJohn;
+// console.log('Is Mark\'s BMI highter than John\'s? ' + moreBmi);
+
+/******************If / Else Statements ****************************/
+
+// var firstName = 'John';
+// var civilStatus = 'single';
+
+// if (civilStatus === 'married') {
+//     console.log(firstName + ' is married!');
+// } else {
+//     console.log(firstName + ' will hopefully marry soon :)');
+// }
+
+// var isMarried = true;
+// if (isMarried) {
+//     console.log(firstName + ' is married!');
+// } else {
+//     console.log(firstName + ' will hopefully marry soon :)');
+// }
+
+// var massMark = 70;
+// var massJohn = 85;
+// var heightMark = 1.7;
+// var heightJohn = 1.8;
+
+// // Both their BMIs
+// var bmiMark = massMark / (heightMark^2);
+// console.log(bmiMark);
+
+// var bmiJohn = massJohn / (heightJohn^2);
+// console.log(bmiJohn);
+
+// if (bmiMark > bmiJohn) {
+//     console.log('Mark\'s BMI is higher than John\'s.');
+// } else {
+//     console.log('John\'s BMI is higher than Mark\'s.');
+// }
+
+// Is Mark's BMI more than John's?
+// moreBmi = bmiMark > bmiJohn;
+// console.log('Is Mark\'s BMI highter than John\'s? ' + moreBmi);
+
+/**************************Boolean Logic ***************************/
+
+// var firstName = 'John';
+// var age = 20;
+
+// if (age < 13) {
+//     console.log(firstName + ' is a boy.');
+// } else if (age >= 13 && age < 20) { // Between 13 and 20 === age >= 13 AND age < 20
+//     console.log(firstName + ' is a teenager.');
+// } else if (age >= 20 && age < 30) {
+//     console.log(firstName + ' is a young man.');
+// } else {
+//     console.log(firstName + ' is a man.');
+// }
+
+/*******The Terbary Operator and Switch Statements *****************/
+
+var firstName = 'John';
+var age = 14;
+
+// Ternary Operator
+age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
+
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+// if (age >= 18) {
+//     var drink = 'beer';
+// } else {
+//     var drink = 'juice';
+// }
+
+// Switch Statement
+var job = 'instructor'
+switch (job) {
+    case 'teacher':
+    case 'instructor':
+        console.log(firstName + ' teaches kids how to code.');
+        break;
+    case 'driver':
+        console.log(firstName + ' drives an uber in Lisbon.');
+        break;
+    case 'designer':
+        console.log(firstName + ' designs beautiful webistes.');
+        break;
+    default:
+        console.log(firstName + ' does something else.');
+}
+
+age = 56;
+
+switch (true) {
+    case age < 13:
+        console.log(firstName + ' is a boy.');
+        break;
+    case age >= 13 && age < 20:
+        console.log(firstName + ' is a teenager.');
+        break;
+    case age >= 20 && age < 30:
+        console.log(firstName + ' is a young man.');
+        break;
+    default:
+        console.log(firstName + ' is a man.');
+}
