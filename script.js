6660521f0e7a81e3c812889310b0aff28c71e957
Ameer Pestana <elearningviet88@gmail.com>
2020-08-04 12:11:57 +0800

    Basic Operators
    Basic Operators

--- script.js
+++ script.js
@@ -19,23 +19,59 @@
 // var johnMark = 'John and Mark';
 // var if = 23;
 
-/***************************Variable mutation and type coercion */
-var firstName = 'John';
-var age = 28;
+/*******************Variable mutation and type coercion ************/
+// var firstName = 'John';
+// var age = 28;
 
-console.log(firstName + ' ' + age);
+// console.log(firstName + ' ' + age);
 
-var job, isMarried;
-job = 'teacher';
-isMarried = false;
+// var job, isMarried;
+// job = 'teacher';
+// isMarried = false;
 
-console.log(firstName + ' is a ' + age + ' year old ' + job + ' . Is he married? ' + isMarried);
+// console.log(firstName + ' is a ' + age + ' year old ' + job + ' . Is he married? ' + isMarried);
 
-// Variable Mutation
-age = 'twenty eight';
-job = 'driver';
+// // Variable Mutation
+// age = 'twenty eight';
+// job = 'driver';
 
-alert(firstName + ' is a ' + age + ' year old ' + job + ' . Is he married? ' + isMarried);
+// alert(firstName + ' is a ' + age + ' year old ' + job + ' . Is he married? ' + isMarried);
 
-var lastName = prompt('What is his last Name?');
-console.log(firstName + ' ' + lastName);
+// var lastName = prompt('What is his last Name?');
+// console.log(firstName + ' ' + lastName);
+
+/***************************Basic Operators *********************/
+// var year, yearJohn, yearMark;
+// now = 2018;
+// ageJohn = 28;
+// ageMark = 33;
+
+// //Math operators
+// yearJohn = now - ageJohn;
+// yearMark = now - ageMark;
+
+// console.log(yearJohn);
+
+// console.log(now + 2);
+// console.log(now * 2);
+// console.log(now / 10);
+
+// //Logical operators
+// var johnOlder = ageJohn > ageMark;
+// console.log(johnOlder);
+
+// //typeof operator
+// console.log(typeof johnOlder);
+// console.log(typeof ageJohn);
+// console.log(typeof 'Mark is older than John');
+// var x;
+// console.log(typeof x);
+
+/********************Operator Procedence **************************/
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+var isFullAge = now - yearJohn >= fullAge;
+console.log(isFullAge);
