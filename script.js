2779e077b5b62db90560c19861ffc8b0562247c4
Ameer Pestana <elearningviet88@gmail.com>
2020-08-08 23:33:53 +0800

    More Functions
    More Functions

--- script.js
+++ script.js
@@ -388,6 +388,32 @@ console.log('Account Available Balance is: ' + balance + '. ');
     console.log(greeting);
 }())
 
+function calculateAge(birthYear){
+    return 2018 - birthYear;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+
+console.log(ageJohn,ageMike,ageJane);
+
+function yearsUntilRetirement(year, firstName){
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+
+    if (retirement > 0){
+        console.log(firstName + ' retires in ' + retirement + ' years.');
+    } else {
+        console.log(firstName + ' is already retired.')
+    }
+    
+}
+
+yearsUntilRetirement(1990,'John');
+yearsUntilRetirement(1948,'Mike');
+yearsUntilRetirement(1969,'Jane');
+
 /***************Function Statements and Expressions ****************/
 
 // Function declaration
@@ -416,6 +442,26 @@ var totalPoints = function(firstName, points){
 }
 
 console.log(totalPoints('John', 555),totalPoints('Mike', 832),totalPoints('Jane', 1001));
+
+//Function declaration
+// function whatDoYouDo(job, firstName){}
+
+var whatDoYouDo = function(job, firstName) {
+    switch(job) {
+        case 'teacher':
+            return firstName + ' teaches kids how to code';
+        case 'driver':
+            return firstName + ' drives a cab in Lisbon';
+        case 'designer':
+            return firstName + ' designs beautiful websites';
+        default:
+            return firstName + ' does something else';
+    }
+}
+
+console.log(whatDoYouDo('teacher','John'));
+console.log(whatDoYouDo('designer','Jane'));
+console.log(whatDoYouDo('retired','Mark'));
 
 /************************** Arrays *********************************/
 
@@ -448,6 +494,37 @@ console.log(john.indexOf(23));
 var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
 console.log(isDesigner);
 
+
+// Initialize new array
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1968, 1948);
+
+console.log(names[2]);
+console.log(names.length);
+
+// Mutate array data
+names[1] = 'Ben';
+names[5] = 'Mary';
+console.log(names);
+
+// Different data types
+var john = ['John', 'Smith', 1990, 'teacher', false];
+
+john.push('blue');
+john.unshift('Mr.')
+console.log(john);
+
+john.pop()
+john.pop()
+john.shift()
+console.log(john);
+
+console.log(john.indexOf(23));
+
+
+var isDesigner = john.indexOf('desginer') === -1 ? 'John is NOT a designer':'John IS a designer';
+console.log(isDesigner);
+
 /************************* Coding Challenge 3 **********************/
 
 /*
@@ -461,3 +538,4 @@ In the end, John would like to have 2 arrays:
 
 (NOTE: To calculate 20% of a value, simply multiply if with 20/100 = 0.2)
 */
+
