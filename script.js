dfaa69ca9ac150e138b994816114af2fe02bb407
Ameer Pestana <elearningviet88@gmail.com>
2020-08-14 23:10:43 +0800

    More Objects
    More Objects

--- script.js
+++ script.js
@@ -916,3 +916,28 @@ var whatDoYouDo = function(job,firstName) {
 console.log(whatDoYouDo('teacher','John'));
 console.log(whatDoYouDo('designer','Jane'));
 console.log(whatDoYouDo('retired','Mark'));
+
+/***************************** Objects *****************************/
+
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane','Mark','Bob','Emily'],
+    job: 'teacher',
+    isMarried: false
+};
+console.log(john.firstName);
+console.log(john['lastName']);
+var x = 'birthYear';
+console.log(john[x]);
+
+john.job = 'designer';
+john['isMarried'] = true;
+console.log(john);
+
+var jane = new Object();
+jane.name = 'Jane';
+jane.birthYear = 1969;
+jane['lastName'] = 'Smith';
+console.log(jane);
