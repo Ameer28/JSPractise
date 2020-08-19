746498892fee86e7c5984ba6b94d400f3e6bb7f6
Ameer Pestana <elearningviet88@gmail.com>
2020-08-19 22:23:51 +0800

    Hoisting & Scoping
    Hoisting & Scoping

--- script.js
+++ script.js
@@ -1074,5 +1074,119 @@ var john = {
     }
 }
 
+var mark = {
+    fullName: 'Mark Miller',
+    bills: [77, 475, 110, 45],
+    calcTips: function() {
+        this.tips = [];
+        this.finalValues = [];
+
+        for (var i = 0; i < this.bills.length; i++){
+            // Determine percentage based on tipping rules
+            var percentage;
+            var bill = this.bills[i];
+
+            if (bill < 100){
+                percentage = .2;
+            } else if (bill >= 100 && bill < 300) {
+                percentage = .1;
+            } else {
+                percentage = .25;
+            }
+
+            // Add results to the corresponding arrays
+            this.tips[i] = bill * percentage;
+            this.finalValues[i] = bill + bill * percentage;
+        }
+    }
+}
+
+function calcAverage(tips){
+    var sum = 0;
+    for (var i = 0; i < tips.length; i++){
+        sum = sum + tips[i];
+    }
+    return sum / tips.length;
+}
+
+// Do the calculations
 john.calcTips();
-console.log(john);
+mark.calcTips();
+console.log(john,mark);
+
+john.average = calcAverage(john.tips);
+mark.average = calcAverage(mark.tips);
+console.log(john, mark);
+
+if(john.average > mark.average) {
+    console.log(john.fullName + '\'s family pays higher tips, with an average of $' + john.average);
+} else if (mark.average > john.average){
+    console.log(mark.fullName + '\'s family pays higher tips, with an average of $' + mark.average);
+} 
+
+
+/*********************** Lecture: Hoisting ***********************/
+
+// Functions
+calculateAge(1965);
+
+function calculateAge(year) {
+    console.log(2016 - year);
+}
+
+
+// retirement(1965);
+var retirement = function(year){
+    console.log(65 - (2016 - year));
+}
+
+// Variables
+console.log(age);
+var age = 23;
+
+function foo() {
+    console.log(age);
+    var age = 65;
+    console.log(age);
+}
+
+foo();
+console.log(age);
+
+/************************ Lecture: Scoping ************************/
+
+// First scoping example
+
+var a = 'Hello! ';
+first();
+
+function first() {
+    var b = 'Hi! ';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+
+// Example to sow the difference between execution stack and scope chain
+
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second(){
+        var c = 'Hey!';
+        third()
+    }
+}
+
+function third(){
+    var d = 'John';
+    //console.log(c);
+    console.log(a+d);
+}
