7f1291fc2209564274a177896c8d00ccd586f717
Ameer Pestana <elearningviet88@gmail.com>
2020-10-04 22:58:25 +0800

    Function Constructor
    Function Constructor

--- script.js
+++ script.js
@@ -1486,3 +1486,34 @@ var whatDoYouDo = function(job, firstName){
 console.log(whatDoYouDo('teacher', 'John'));
 console.log(whatDoYouDo('designer', 'Jane'));
 console.log(whatDoYouDo('retired', 'Mark'));
+
+/****************************** Function Constructor *******************************/
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    job: 'teacher'
+};
+
+var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+    }
+
+Person.prototype.calculateAge = function() {
+    console.log(2016 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
